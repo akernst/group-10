40b00f8b362dcb801e664b112c4553c81c96225d
Alex Ernst <akernst162@gmail.com>
2016-08-01 16:20:38 -0500

    good work team

--- volunteer-project/event.py
+++ volunteer-project/event.py
@@ -4,9 +4,9 @@ from google.appengine.ext import ndb
 class Event(ndb.Model):
 	eventname = ndb.StringProperty(required = True)
 	datetime = ndb.DateTimeProperty(auto_now = True)
+	eventdate = ndb.StringProperty(required=True)
 	eventinfo = ndb.StringProperty(required = True)
-	location = ndb.GeoPtProperty(required = True)
+	location = ndb.GeoPtProperty(required = False)
 	agereq = ndb.IntegerProperty(required = False)
-	tags = ndb.StringProperty(required = True)
-	dateadded = ndb.DateProperty(required = True)
-	profile = ndb.BlobProperty(required = True)
+	tags = ndb.StringProperty(required=False)
+	profile = ndb.BlobProperty(required = False)
