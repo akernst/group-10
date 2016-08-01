e3c90f0737d7ba0820c44999c08f1259b57f8de1
lucyyloo22 <yueting_wang@college.harvard.edu>
2016-08-01 16:56:15 -0500

    event changed

--- volunteer-project/event.py
+++ volunteer-project/event.py
@@ -5,16 +5,8 @@ class Event(ndb.Model):
 	eventname = ndb.StringProperty(required = True)
 	eventdate = ndb.StringProperty(required = True)
 	datetime = ndb.DateTimeProperty(auto_now = True)
-<<<<<<< HEAD
-	eventinfo = ndb.StringProperty(required = False)
-	location = ndb.StringProperty(required = False)
-	agereq = ndb.IntegerProperty(required = False)
-	tags = ndb.StringProperty(required = True)
-=======
-	eventdate = ndb.StringProperty(required=True)
 	eventinfo = ndb.StringProperty(required = True)
-	location = ndb.GeoPtProperty(required = False)
-	agereq = ndb.IntegerProperty(required = False)
+	location = ndb.IntegerProperty(required = False)
+	agereq = ndb.IntegerProperty(required = True)
 	tags = ndb.StringProperty(required=False)
->>>>>>> 665e6aec4c8f7152cc027917eafd5fa513b873ed
 	profile = ndb.BlobProperty(required = False)
