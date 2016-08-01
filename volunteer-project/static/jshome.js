3bdf05eba8472854102013160ff77f3714927c3e
Alex Ernst <akernst162@gmail.com>
2016-08-01 17:47:06 -0500

    hey

--- volunteer-project/static/jshome.js
+++ volunteer-project/static/jshome.js
@@ -6,8 +6,12 @@ function redirectToEvent(){
     window.location.replace("/createEvent")
 }
 
+function redirectToSearch(){
+    window.location.replace("/search")
+}
 function setup() {
     $("#login").click(redirectToLogin);
     $("#event").click(redirectToEvent);
+    $("#search").submit(redirectToSearch);
 }
 $(document).ready(setup)
