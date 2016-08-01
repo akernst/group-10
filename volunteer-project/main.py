561597085bdb2b746fe7e0899066655a1ba21ed9
lucyyloo22 <yueting_wang@college.harvard.edu>
2016-08-01 10:58:24 -0500

    added login to main.py

--- volunteer-project/main.py
+++ volunteer-project/main.py
@@ -16,6 +16,7 @@
 #
 import webapp2
 import jinja2
+from google.appengine.api import users
 
 env = jinja2.Environment(
 	loader=jinja2.FileSystemLoader("templates"))
@@ -24,6 +25,18 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
     	template = env.get_template("home.html")
         self.response.write(template.render())
+
+        #initiates login
+        user = users.get_current_user()
+    	if user:
+        	greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+            	(user.nickname(), users.create_logout_url('/')))
+    	else:
+        	greeting = ('<a href="%s">Sign in or register</a>.' %
+            	users.create_login_url('/'))
+
+    	self.response.out.write('<html><body>%s</body></html>' % greeting)
+       
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
