3ba5c6980a09fcda8681bd2546c0995d4a4a1d41
lucyyloo22 <yueting_wang@college.harvard.edu>
2016-08-01 11:11:59 -0500

    login handler home redirect

--- volunteer-project/main.py
+++ volunteer-project/main.py
@@ -23,21 +23,29 @@ env = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-    	template = env.get_template("home.html")
-        self.response.write(template.render())
 
         #initiates login
         user = users.get_current_user()
     	if user:
         	greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
             	(user.nickname(), users.create_logout_url('/')))
+        	self.redirect('/home')
+
     	else:
         	greeting = ('<a href="%s">Sign in or register</a>.' %
             	users.create_login_url('/'))
 
     	self.response.out.write('<html><body>%s</body></html>' % greeting)
-       
+
+class HomeHandler(webapp2.RequestHandler):
+	def get(self):
+		template = env.get_template("home.html")
+        self.response.write(template.render())
+
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/home', HomeHandler)
 ], debug=True)
