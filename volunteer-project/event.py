0fe9b96f7074b407e9a78baf51064689ebdb1947
Alex Ernst <akernst162@gmail.com>
2016-08-02 10:43:49 -0500

    hey

--- volunteer-project/event.py
+++ volunteer-project/event.py
@@ -5,7 +5,7 @@ class Event(ndb.Model):
 	eventname = ndb.StringProperty(required = True)
 	eventdate = ndb.StringProperty(required = True)
 	datetime = ndb.DateTimeProperty(auto_now = True)
-	eventinfo = ndb.StringProperty(required = False)
+	eventinfo = ndb.StringProperty(required = True)
 	location = ndb.IntegerProperty(required = False)
 	agereq = ndb.IntegerProperty(required = True)
 	tags = ndb.StringProperty(required= False)
