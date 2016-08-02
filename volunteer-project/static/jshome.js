cca667ef708c625b79728907a29712c1832a9ebb
lucyyloo22 <yueting_wang@college.harvard.edu>
2016-08-02 09:08:05 -0500

    hi

--- volunteer-project/static/jshome.js
+++ volunteer-project/static/jshome.js
@@ -6,8 +6,6 @@ function redirectToEvent(){
     window.location.replace("/createEvent")
 }
 
-
-
 function setup() {
     $("#login").click(redirectToLogin);
     $("#event").click(redirectToEvent);
