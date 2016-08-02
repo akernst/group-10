ee3c753c2c582bd7fe2d24e9522d03d46381b450
lucyyloo22 <yueting_wang@college.harvard.edu>
2016-08-02 11:13:17 -0500

    hi

--- volunteer-project/voluser.py
+++ volunteer-project/voluser.py
@@ -4,5 +4,6 @@ from google.appengine.ext import ndb
 class VolUser(ndb.Model):
 	name = ndb.StringProperty(required = True)
 	email = ndb.StringProperty(required = True)
-	age = ndb.IntegerProperty(required = True)
+	age = ndb.IntegerProperty(required = False)
 	organizationinfo = ndb.StringProperty(required = False)
+	avatar = ndb.BlobProperty(required = False)
